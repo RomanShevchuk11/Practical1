40dc17540537d89f49a7ee0f9e1367a0c59245fa
Roman <rshewchukk@gmail.com>
2025-11-22 16:24:54 +0200

    Add Practical1

---
+++ Practical1/Task2/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace Task2
+{
+    public class Program
+    {
+        public static int[] GenerateRandomArray(int size, int min, int max)
+        {
+            int[] array = new int[size];
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(min, max);
+            }
+            return array;
+        }
+
+        public static int GetSum(int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+        public static double GetAverage(int[] numbers)
+        {
+            double sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            double avg = sum / numbers.Length;
+            return avg;
+        }
+        
+        public static int GetMin(int[] numbers)
+        {
+            int min = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+            return min;
+        }
+
+        public static
+            int GetMax(int[] numbers)
+        {
+            int max = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+            return max;
+        }
+        public static void Main(string[] args)
+        {
+            
+            
+            
+            
+            
+
+                int[] array = GenerateRandomArray(10, 1, 100);
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(" " + array[i]);
+                }
+                Console.WriteLine();
+                int sum = GetSum(array);
+                Console.WriteLine("Сума масиву: " + sum);
+
+                double avg = GetAverage(array);
+                Console.WriteLine("Середнє значення: " + avg);
+
+                int max = GetMax(array);
+                Console.WriteLine("Максимальне число: " + max);
+
+                int min = GetMin(array);
+                Console.WriteLine("Мінімальне число: " + min);
+            
+        }
+    }
+}
---
+++ Practical1/Task5/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Task5
+{
+    public class Program
+    {
+
+        public static double GetAverage(int[] marks)
+        {
+            double sum = 0;
+            foreach (int mark in marks)
+            {
+                sum += mark;
+            }
+            double avg = sum / marks.Length;
+            return avg;
+        }
+
+        public static int GetMin(int[] marks)
+        {
+            int min = marks[0];
+            foreach (int mark in marks)
+            {
+                if (min > mark)
+                {
+                    min = mark;
+                }
+            }
+            return min;
+        }
+
+        public static int GetMax(int[] marks)
+        {
+            int max = marks[0];
+            foreach (int mark in marks)
+            {
+                if (max < mark)
+                {
+                    max = mark;
+                }
+            }
+            return max;
+        }
+
+        public static void PrintGroupStatistics(int[][] groups)
+        {
+            for (int i = 0; i < groups.Length; i++)
+            {
+                int[] group = groups[i];
+                Console.WriteLine("Група " + (i + 1) + ": " +
+                    "Середній = " + GetAverage(group) + ", " +
+                    "Мінімальний = " + GetMin(group) + ", " +
+                    "Максимальний = " + GetMax(group));
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            
+            
+           
+
+            
+            
+                int[][] marks =
+                {
+                  new int[] { 2, 3, 5 },
+                  new int[] { 6, 7, 8 },
+                  new int[] { 3, 4, 5 }
+            };
+
+                PrintGroupStatistics(marks);
+
+            
+        }
+    }
+}
